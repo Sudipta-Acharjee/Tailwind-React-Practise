f8f888844432d0f2514dea357a9e5924fa3ae21b
unknown <sudiptoacharjee4@gmail.com>
2021-09-29 22:24:32 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="container bg-green-500 text-center p-5 ml-10">
+      App container
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1>Hello, Welcome to my blog</h1>
+            <p>Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+            <p>Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+            <p>Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+    )
+}
+
+export default Home
