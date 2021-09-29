22bd87c81b550282ef9d1e34d5925cb31e6187c7
unknown <sudiptoacharjee4@gmail.com>
2021-09-29 22:28:55 +0600

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="mb-20">
-            <h1>Hello, Welcome to my blog</h1>
-            <p>Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-            <p>Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-            <p>Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+            <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">Hello, Welcome to my blog</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
     )
 }
