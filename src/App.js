0b61faef8577d6ef36831992ceada6f486e925d7
unknown <sudiptoacharjee4@gmail.com>
2021-09-29 22:38:11 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
-      App container
-      <Home />
-    </div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
