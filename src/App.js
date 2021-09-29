f15606eac9cbdc3816bb2e5eecf560063bea802d
unknown <sudiptoacharjee4@gmail.com>
2021-09-29 22:31:33 +0600

    center

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="container bg-green-500 text-center p-5 ml-10">
+    <div className="max-w-screen-md mx-auto pt-20">
       App container
       <Home />
     </div>
