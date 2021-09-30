0a6d2c6fbdd661672f666bee7e9fcb9d1012b692
unknown <sudiptoacharjee4@gmail.com>
2021-09-30 10:24:28 +0600

    creating route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './pages/About';
+import ArticleList from './pages/ArticleList';
+import Article from './pages/Article';
 
 function App() {
   return (
     <Router>
       <div className="max-w-screen-md mx-auto pt-20">
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/articleList" component={ArticleList} />
+        <Route exact path="/article" component={Article} />
+
       </div>
     </Router>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Home = () => {
+const about = () => {
     return (
         <div className="mb-20">
-            <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">Home</h1>
+            <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">About</h1>
             <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
             <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
             <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
@@ -11,4 +11,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default about
