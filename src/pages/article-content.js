b385adef48a59f8ca2f571ed91a7cb7f9bd2f4f7
unknown <sudiptoacharjee4@gmail.com>
2021-09-30 10:57:19 +0600

    updated

---
+++ src/pages/article-content.js
@@ -0,0 +1,8 @@
+const articles = [
+    {
+        name: "learn-react",
+        title: "The fastest way to learn React",
+        content: [`Welcome! Today we're going to be talking about the fastest way to learn React.`],
+    },
+];
+export default articles;
