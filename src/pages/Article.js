5c3ec0a1aa97e71e9f3fc095fce359f734961268
unknown <sudiptoacharjee4@gmail.com>
2021-10-01 11:16:54 +0600

    map done

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import articleContent from './article-content'
 const Article = ({ match }) => {
-    const name = match.params.name
+    const name = match.params.name;
+    const article = articleContent.find((article) => article.name === name);
     return (
         <div className="mb-20">
-            <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">This is the {name} of Article</h1>
-            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
+            <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">{article.title}</h1>
+            {article.content.map((paragraph, index) => (
+                <p className="mx-auto leading-relaxed text-base mb-4" key={index}>{paragraph}</p>
+            ))}
         </div>
     )
 }
