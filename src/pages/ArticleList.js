5722b108b0e2280f09ea01a672508e2ef8f2a677
unknown <sudiptoacharjee4@gmail.com>
2021-10-01 13:15:14 +0600

    updated

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import article from './article-content';
+import articleContent from './article-content';
 import { Link } from "react-router-dom"
 
 const ArticleList = () => {
@@ -8,7 +8,7 @@ const ArticleList = () => {
             <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">Article List</h1>
             <div className="container py-4 mx-auto">
                 <div className="flex flex-wrap m-4">
-                    {article.map((article, index) => (<div key={index} className="p-4 md:w-1/2">
+                    {articleContent.map((article, index) => (<div key={index} className="p-4 md:w-1/2">
                         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                             <Link to={`/article/${article.name}`}>
                                 <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={article.thumbnail} alt='blog'></img>
@@ -23,11 +23,10 @@ const ArticleList = () => {
                                     {article.content[0].substring(0, 115)}...
                                 </p>
                                 <div className="flex item-corner flex-wrap">
-                                    <Link className="flex-bg-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" to={`/article/${article.name}}>
-                                    Learn more...
+                                    <Link className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" to={`/article/${article.name}`}>
+                                        Learn more...
                                     </Link>
                                 </div>
-                            </div>
                             </div>
                         </div>
                     </div>))}
