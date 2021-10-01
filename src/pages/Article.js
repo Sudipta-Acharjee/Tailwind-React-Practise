5d18463a596d6778445a75eabba8540b287b07a2
unknown <sudiptoacharjee4@gmail.com>
2021-10-01 15:17:09 +0600

    updated

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -6,7 +6,7 @@ const Article = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find((article) => article.name === name);
     if (!article) return <h1>Article not found</h1>;
-    const otherArticle = articleContent.filter((article) => article.name !=== name);
+    const otherArticle = articleContent.filter((article) => article.name !== name);
     return (
         <div className="mb-20">
             <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">{article.title}</h1>
