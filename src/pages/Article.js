b4f6948aa6b38e01c98af7d573b9a174439a5958
unknown <sudiptoacharjee4@gmail.com>
2021-10-01 11:28:06 +0600

    updated

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import articleContent from './article-content'
+import React from 'react';
+import articleContent from './article-content';
+
+
 const Article = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find((article) => article.name === name);
+    if (!article) return <h1>Article not found</h1>;
     return (
         <div className="mb-20">
             <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">{article.title}</h1>
