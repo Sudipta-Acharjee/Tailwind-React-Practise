5720a19b581cad3aa7ce4c3a2e5dffabe88bde32
unknown <sudiptoacharjee4@gmail.com>
2021-10-01 11:31:51 +0600

    updated

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import article from './articleContent';
 
 const ArticleList = () => {
     return (
-        <div className="mb-20">
-            <h1 className="sm:text-4xl text-2xl font-boldmt-6 text-gray-900">ArticleList</h1>
-            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-            <p className="mx-auto leading-relaxed text-base mb-4">Lorem Inpsum dolor sit amet, consectetur adipiscing elit</p>
-        </div>
+        <>
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">Article List</h1>
+            <div className="container py-4 mx-auto">
+                <div className="flex flex-wrap m-4">
+                    {article.map((article, index) => (<div key={index} className="p-4 md:w-1/2">
+                        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"></div>
+                    </div>))}
+                </div>
+            </div>
+        </>
     )
 }
 
