c0d1acf4c3b76132155530f538fd4e41fb314e31
unknown <sudiptoacharjee4@gmail.com>
2021-10-02 09:02:21 +0600

    updated

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -5,10 +5,10 @@ import articleContent from './article-content';
 const ArticleList = () => {
     return (
         <>
-            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">Article List</h1>
-            <div className="container py-4 mx-auto">
-                <div className="flex flex-wrap -m-4">
-                    <Articles articles={articleContent} />
+            <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>Article List</h1>
+            <div className=' container py-4 mx-auto'>
+                <div className='flex flex-wrap -m-5'>
+                    <div><Articles articles={articleContent} /></div>
                 </div>
             </div>
         </>
